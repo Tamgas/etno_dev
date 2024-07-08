6c8a96ea83aacb58ecbb9c0f455cb9b91e3da81c
Elzar <chynarbekov07@gmail.com>
2024-07-08 17:12:22 +0600

    herolordu buturdum

--- src/components/Vacancies/index.jsx
+++ src/components/Vacancies/index.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Vacancies = () => {
-    return (
-        <div>
-            emir1
-        </div>
-    );
-};
+const Vacancy = () => {
+	return (
+		<div id='vacansies'>
+			<div className='vacansies'>
+				<div className='btn'>
+					<button>Дабавить вакасию</button>
+				</div>
+			</div>
+			<div className='bloc'>
+				<Link to={'/'}>
+					<div className='text-nav'>
+						<h4>
+							Компония{' '}
+							<span>
+								OcOO "Грин Телеком <br /> Сервис" (O!Dengi)
+							</span>
+						</h4>
+						<h4>
+							Должность <span>Дата инженер</span>
+						</h4>
+						<h4>
+							Оклад <span>От 90000 KGS в месяц</span>
+						</h4>
+						<h4>
+							Тип <span>Офис / Бишкек</span>
+						</h4>
+					</div>
+				</Link>
+				<div className='image'>
+					<img
+						data-v-7ad410c8=''
+						src='https://static.qiwi.com/img/providers/logoBig/36356_l.png'
+						alt='img'
+					/>
+				</div>
+			</div>
 
-export default Vacancies;
+			<div className='bloc2'>
+				<Link to={'/'}>
+					<div className='text-nav2'>
+						<h4>
+							Компония{' '}
+							<span>
+								OcOO Mega
+								<br /> Сервис" (MegaPay)
+							</span>
+						</h4>
+						<h4>
+							Должность <span>Менеджер</span>
+						</h4>
+						<h4>
+							Оклад <span>От 50000 KGS в месяц</span>
+						</h4>
+						<h4>
+							Тип <span>Офис / Бишкек</span>
+						</h4>
+					</div>
+				</Link>
+				<div className='image2'>
+					<img
+						data-v-7ad410c8=''
+						src='https://www.megacom.kg/uploads/ckeditor/pictures/3631/content_ru_MEGA-New-1200x900.png'
+						alt='img'
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Vacancy
